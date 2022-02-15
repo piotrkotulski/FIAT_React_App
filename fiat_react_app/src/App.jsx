f32b8e18af2eb443120c44f93da03f52f7e65e71
piotrkotulski <93822027+piotrkotulski@users.noreply.github.com>
2022-02-15 15:17:08 +0100

    first_version

---
+++ fiat_react_app/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CurrencyApp from './components/CurencyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <CurrencyApp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fiat_react_app/src/components/CurencyApp.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+
+const CurrencyApp = () => {
+  const initChosenAmount = 1;
+  const initChosenCurrency = "USD";
+  const initCurrencies = [];
+  const [chosenAmount, setChosenAmount] = useState(initChosenAmount);
+  const [chosenCurrency, setChosenCurrency] = useState(initChosenCurrency);
+  const [currencies, setCurrencies] = useState(initCurrencies);
+  const NBP_API_URL =
+    "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+
+  useEffect(() => {
+    fetch(NBP_API_URL)
+      .then((response) => response.json())
+      .then((data) => setCurrencies(data[0].rates))
+      .catch((err) => console.error(err));
+  }, []);
+
+  // functions
+  
+  const calculatedCurrency = () => {
+    const chosenCurrencyValue = currencies.find(
+      ({ code }) => code === chosenCurrency
+    )?.mid;
+    
+    return (chosenAmount * chosenCurrencyValue).toFixed(4);
+  };
+
+  // events
+  const changeChosenAmount = (e) => setChosenAmount(e.target.value);
+  const changeChosenCurrency = (e) => setChosenCurrency(e.target.value);
+
+  return (
+    <div className="container">
+      <h1>PRZELICZNIK KURSÓW WALUT </h1>
+      <p>Sprawdź dzisiejsze kursy </p>
+      <div className="values">
+        <div className="values-left">
+          <input
+            type="number"
+            onChange={changeChosenAmount}
+            value={chosenAmount}
+          />
+          <select onChange={changeChosenCurrency} value={chosenCurrency}>
+            {currencies.map(({ code }) => (
+              <option key={code} value={code}>
+                {code}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button onClick={calculatedCurrency()} className="exchange">
+                <i class="far fa-arrow-alt-circle-right"></i>
+            </button>
+        <div className="values-right">
+          <input type="number" className="amount2"  disabled />
+          <select id="currency2">
+          <option value="PLN">PLN</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyApp;
